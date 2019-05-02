e7ff3edfacf993462ae0d87effe948e44b310e22
liangxiegame <liangxiegame@163.com>
2019-05-02 18:57:05 +0800

    v0.0.1.0 环境创建

---
+++ Assets/Scripts/App.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.UIWidgets.engine;
+using Unity.UIWidgets.material;
+using Unity.UIWidgets.widgets;
+
+namespace HabitApp
+{
+    public class App : UIWidgetsPanel
+    {
+        protected override Widget createWidget()
+        {
+            return new MaterialApp(
+                home: new Scaffold(
+                    appBar: new AppBar(
+                        title: new Text("Title")
+                    ),
+                    body: new Text("Body")
+                )
+            );
+        }
+    }
+}
