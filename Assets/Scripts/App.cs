680e2b5dcd100069ca20c1b68637d2b55b86fa3e
liangxiegame <liangxiegame@163.com>
2019-05-02 19:36:13 +0800

    v0.0.1.1 习惯的创建和显示完成

--- Assets/Scripts/App.cs
+++ Assets/Scripts/App.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Unity.UIWidgets.engine;
 using Unity.UIWidgets.material;
+using Unity.UIWidgets.painting;
 using Unity.UIWidgets.widgets;
+using UnityEngine;
 
 namespace HabitApp
 {
@@ -15,9 +17,52 @@ namespace HabitApp
                     appBar: new AppBar(
                         title: new Text("Title")
                     ),
-                    body: new Text("Body")
+                    body: new Body()
                 )
             );
+        }
+    }
+
+    public class Body : StatefulWidget
+    {
+        public override State createState()
+        {
+            return new BodyState();
+        }
+    }
+
+    class BodyState : State<Body>
+    {
+        List<string> mHabits = new List<string>();
+
+        public override Widget build(BuildContext context)
+        {
+            return
+                new Column(
+                    children: new List<Widget>()
+                    {
+                        new Container(
+                            color:Colors.grey,
+                            height:200,
+                            child: new ListView(
+                                children: mHabits.Select(habit => new Text(habit) as Widget).ToList()
+                            )
+                        ),
+                        new Container(
+                            alignment: Alignment.center,
+                            child: new FlatButton(
+                                color: Colors.blue,
+                                child: new Text("创建习惯", style: new TextStyle(color: Colors.white)),
+                                onPressed:
+                                () =>
+                                {
+                                    this.setState(() => { mHabits.Add("新的习惯"); });
+                                }
+                            )
+                        )
+                    }
+
+                );
         }
     }
 }
